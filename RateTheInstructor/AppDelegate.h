f977df0cf65af5bc51c5286d24aee5fb0bab0254
Giang Pham <giangttpham@gmail.com>
2014-11-12 13:33:25 -0800

    Initial Commit

---
+++ RateTheInstructor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RateTheInstructor
+//
+//  Created by Tra` Beo' on 11/12/14.
+//  Copyright (c) 2014 Giang Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
